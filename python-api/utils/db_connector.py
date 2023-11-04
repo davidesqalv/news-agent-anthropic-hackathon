56a7c0fbf313e9567f9469af34f138f7109040b0
Jan Ebbing <Jan.Ebbing@gmx.de>
2023-11-04 17:02:03 +0000

    backend: Start on DB connector. To be merged with Davids changes

--- python-api/utils/db_connector.py
+++ python-api/utils/db_connector.py
@@ -1,2 +1,7 @@
 class DBConnector:
-    pass
+    DB_NAME_USER_PROFILES = "user-profiles"
+    DB_NAME_INCOMING_DATA = "incoming-data"
+    DB_NAME_GENERATED_DIGESTS = "generated-digests"
+
+    def __init__(self) -> None:
+        pass
