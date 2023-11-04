d73d84fa54bbabca9f57d42e915c62564e4903ec
David Esquivel <devdavesq@gmail.com>
2023-11-04 16:51:00 +0000

    mongo base connection

--- python-api/app.py
+++ python-api/app.py
@@ -3,8 +3,23 @@ from typing import Annotated
 from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import find_dotenv, load_dotenv
+from os import environ as env
+
+
 app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+# Loading env variables
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+# Set up a connection to your MongoDB database using Motor
+client = AsyncIOMotorClient(env.get("MONGO_CLIENT"))
+user_collection = client["user-profiles"]
 
 
 @app.get("/")
@@ -22,9 +37,20 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
 @app.get("/preferences")
 async def get_preferences(token: Annotated[str, Depends(oauth2_scheme)]):
     # TODO: verify the token
-    preferences = [
-        "Artificial Intelligence",
-        "UK Politics",
-        "Russia-Ukraine War",
-    ]  # TODO: Look up the user's preferences in the DB
+
+    # preferences = [
+    #     "Artificial Intelligence",
+    #     "UK Politics",
+    #     "Russia-Ukraine War",
+    # ]  # TODO: Look up the user's preferences in the DB
+
+    projection = {"_id": 0, "preferences": 1}
+
+    user_data = await user_collection.find_one({"username": "test"}, projection)
+
+    preferences = user_data.get("preferences")
+
+    # for testing purposes
+    print(f"preferences: {preferences}")
+
     return preferences
