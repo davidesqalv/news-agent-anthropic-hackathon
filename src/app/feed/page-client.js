97e2346efa8c91ced8f64ebbc1d63856a1fdd665
David Esquivel <devdavesq@gmail.com>
2023-11-05 03:00:56 +0000

    some ui changes

--- src/app/feed/page-client.js
+++ src/app/feed/page-client.js
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 
 const containerVariants = {
@@ -13,7 +13,19 @@ const containerVariants = {
     opacity: 1,
     transition: {
       delay: 0.2,
+      staggerChildren: 0.2, // add stagger effect
     },
+  },
+};
+
+const articleVariants = {
+  hidden: {
+    opacity: 0,
+    // y: 20,
+  },
+  visible: {
+    opacity: 1,
+    // y: 0,
   },
 };
 
@@ -21,34 +33,58 @@ const articles = [
   {
     title: "Article 1",
     description: "Description for article 1",
-    link: "https://example.com/article1",
+    link: "https://www.reuters.com/world/middle-east/arab-world-us-split-gaza-ceasefire-israeli-offensive-presses-2023-11-05/",
   },
   {
     title: "Article 2",
     description: "Description for article 2",
-    link: "https://example.com/article2",
+    link: "https://www.nytimes.com/2023/11/04/us/politics/trump-desantis-florida.html",
   },
   {
     title: "Article 3",
     description: "Description for article 3",
-    link: "https://example.com/article3",
+    link: "https://www.bbc.co.uk/news/uk-england-devon-67311250",
   },
 ];
+
+async function getArticleImage(articleUrl) {
+  const apiKey = "pk_70fe81f7aea300606721c526258fd22558ca2738";
+  const apiUrl = `https://jsonlink.io/api/extract?url=${articleUrl}&api_key=${apiKey}`;
+
+  const response = await fetch(apiUrl);
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status} - ${response.statusText}`);
+  }
+  const responseJson = await response.json();
+
+  //   console.log(responseJson);
+  return responseJson.images[0];
+}
 
 export default function FeedClient() {
   const [votes, setVotes] = useState(articles.map(() => 0));
   const [images, setImages] = useState(articles.map(() => ""));
 
+  useEffect(() => {
+    async function fetchImages() {
+      const newImages = [];
+      for (let i = 0; i < articles.length; i++) {
+        const imageUrl = await getArticleImage(articles[i].link);
+        newImages.push(imageUrl);
+      }
+      setImages(newImages);
+    }
+    fetchImages();
+  }, []);
+
+  useEffect(() => {
+    console.log(images);
+  }, [images]);
+
   const handleVote = (index, value) => {
     const newVotes = [...votes];
     newVotes[index] += value;
     setVotes(newVotes);
-  };
-
-  const handleImageLoad = (index, src) => {
-    const newImages = [...images];
-    newImages[index] = src;
-    setImages(newImages);
   };
 
   return (
@@ -58,12 +94,18 @@ export default function FeedClient() {
       initial="hidden"
       animate="visible"
     >
-      <h1 className="text-3xl font-bold text-white font-serif">News Feed</h1>
+      <h1 className="text-3xl font-bold text-white font-serif">Feed</h1>
+
       <div className="grid grid-cols-1 gap-4 w-full max-w-6xl">
+        <p className="text-white text-center">
+          Here's your ritual AI curated feed. You can upvote or downvote
+          articles to help us learn your preferences.
+        </p>
         {articles.map((article, index) => (
-          <div
+          <motion.div // wrap article in motion.div
             key={index}
             className="bg-gray-800 rounded-lg overflow-hidden shadow-md flex flex-row items-center space-x-4 p-4"
+            variants={articleVariants} // add variants
           >
             <div className="relative h-16 w-16">
               {images[index] ? (
@@ -80,18 +122,10 @@ export default function FeedClient() {
                   className="w-full h-full object-cover"
                 />
               )}
-              <img
-                src={article.link + "/og-image.jpg"}
-                alt={article.title}
-                className="hidden"
-                onLoad={() =>
-                  handleImageLoad(index, article.link + "/og-image.jpg")
-                }
-              />
             </div>
             <div className="flex flex-col w-full">
               <a href={article.link} target="_blank" rel="noopener noreferrer">
-                <h2 className="text-lg font-bold text-white font-serif">
+                <h2 className="text-xl font-bold text-white font-serif">
                   {article.title}
                 </h2>
               </a>
@@ -115,7 +149,7 @@ export default function FeedClient() {
                 </div>
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </motion.div>
